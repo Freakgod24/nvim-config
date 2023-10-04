127a7880a4144b8333056b94fd6a0c89651eb268
Freakgod24 <dany.newby@gmail.com>
2023-10-04 02:42:24 +0000

    Update Lsp with rust

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -2,9 +2,23 @@ local base = require("plugins.configs.lspconfig")
 
 local on_attach = base.on_attach
 local capabilities = base.capabilities
-
 local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
 
+-- Rust lspconfig
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"rust"},
+    root_dir = util.root_pattern("Cargo.toml"),
+    settings = {
+        ['rust-analyzer'] = {
+            cargo = {
+                allFeatures = true,
+            },
+        },
+    },
+})
 -- C/C++ lspconfig
 lspconfig.clangd.setup({
   on_attach = function(client, bufnr)
