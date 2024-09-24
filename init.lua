3017ea619452c6c27f69a82831f06dfb62fefc8d
Freakgod24 <dany.newby@gmail.com>
2024-09-24 13:46:08 -0400

    Adding autochdir

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.autochdir = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
