86221869eb94efd0ff7cfd2162a6d1162e28dd88
Dany Newby <dnewby@briowireless.com>
2024-10-22 15:46:01 -0400

    Remove $HOME from SPC f f in dashboard

--- init.lua
+++ init.lua
@@ -122,7 +122,7 @@ require("lazy").setup({
 
 			dashboard.section.buttons.val = {
 				dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("SPC f f", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
+				dashboard.button("SPC f f", "󰮗   Find file", ":Telescope find_files<CR>"),
 				dashboard.button("SPC f o", "   Recent", ":Telescope oldfiles<CR>"),
 				dashboard.button(
 					"SPC f c",
