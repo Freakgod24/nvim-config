2ba2225d443d1c81a9983675a89c8e44d034e82a
Freakgod24 <dany.newby@gmail.com>
2025-01-02 15:10:49 -0500

    Fix: Set transparency true in TokyoNight

--- lua/freakgod/plugins/tokyonight.lua
+++ lua/freakgod/plugins/tokyonight.lua
@@ -1,18 +1,15 @@
-return
-{
-	{                -- Theme
-		"folke/tokyonight.nvim",
-		priority = 1000, -- First to load
-		init = function()
-			vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd.hi("Comment gui=none")
-		end,
-		opts = {
+return {
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
 			transparent = true,
 			styles = {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
-		},
-	}
+		})
+
+		vim.cmd("colorscheme tokyonight")
+	end,
 }
