5c63b52455a5060d66091f9594441fbc8d1791bc
Freakgod24 <dany.newby@gmail.com>
2025-01-02 15:05:35 -0500

    Adding noice.nvim

---
+++ lua/freakgod/plugins/noice.lua
@@ -0,0 +1,31 @@
+return {
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<Esc>", vim.cmd.NoiceDismiss, desc = "󰎟 Clear notifications" },
+		},
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- "rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				cmdline = {
+					view = "cmdline",
+					format = {
+						cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+					},
+				},
+			})
+		end,
+	},
+}
