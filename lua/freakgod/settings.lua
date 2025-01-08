1dd19f3c862ff79dae4bc742ecec37bda13f0669
DS\da097953 <dany.newby@mda.space>
2025-01-08 16:01:52 -0500

    Fix: Disable ~ chars in buffer and Disable cursor line
    Was ugly in the debugger window.

--- lua/freakgod/settings.lua
+++ lua/freakgod/settings.lua
@@ -22,8 +22,9 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.fillchars = { eob = ' ' }
 vim.opt.inccommand = "split"
-vim.opt.cursorline = true
+-- vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
