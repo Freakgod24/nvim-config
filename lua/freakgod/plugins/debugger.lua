8a5b2da5f7acfd7429a7d5ab25eeec606f1959fd
DS\da097953 <dany.newby@mda.space>
2025-01-08 16:01:04 -0500

    Add nvim-dap for Python

---
+++ lua/freakgod/plugins/debugger.lua
@@ -0,0 +1,82 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+			"mfussenegger/nvim-dap-python",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			local dap_python = require("dap-python")
+
+			require("dapui").setup({})
+			require("nvim-dap-virtual-text").setup({
+				commented = true, -- Show virtual text alongside comment
+			})
+
+			dap_python.setup("pythonw")
+
+			vim.fn.sign_define("DapBreakpoint", {
+				text = "",
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
+
+			vim.fn.sign_define("DapBreakpointRejected", {
+				text = "", -- or "❌"
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
+
+			vim.fn.sign_define("DapStopped", {
+				text = "", -- or "→"
+				texthl = "DiagnosticSignWarn",
+				linehl = "Visual",
+				numhl = "DiagnosticSignWarn",
+			})
+
+			-- Automatically open/close DAP UI
+			-- dap.listeners.after.event_initialized["dapui_config"] = function()
+			-- 	dapui.open()
+			-- end
+
+			local opts = { noremap = true, silent = true }
+
+			vim.keymap.set("n", "<leader>db", function()
+				dap.toggle_breakpoint()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>dc", function()
+				dap.continue()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>do", function()
+				dap.step_over()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>di", function()
+				dap.step_into()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>dO", function()
+				dap.step_out()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>dq", function()
+				require("dap").terminate()
+			end, opts)
+
+			vim.keymap.set("n", "<leader>du", function()
+				dapui.toggle()
+				vim.cmd("highlight! link StatusLineNC NormalNC") -- Icons glitch fix
+			end, opts)
+
+			vim.keymap.set({ "n", "v" }, "<leader>ds", "<ESC>:lua require('dap-python').debug_selection()<CR>", opts)
+		end,
+	},
+}
