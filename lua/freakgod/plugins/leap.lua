fed697d70927fbdd681ec8e5dfa93aa3c70c5dfe
DS\da097953 <dany.newby@mda.space>
2025-01-09 12:15:32 -0500

    Fix: Setting offset=2 in leap
    Allows the cursor to be at the end instead of the beginning.

--- lua/freakgod/plugins/leap.lua
+++ lua/freakgod/plugins/leap.lua
@@ -5,7 +5,12 @@ return {
 			require("leap").opts.preview_filter = function()
 				return false
 			end
-			vim.keymap.set({ "n" }, "s", "<Plug>(leap)")
+			vim.keymap.set({ "n" }, "s", function()
+				require('leap').leap {
+					offset = 2,
+					target_windows = { vim.api.nvim_get_current_win() }
+				}
+			end)
 		end,
 	},
 }
