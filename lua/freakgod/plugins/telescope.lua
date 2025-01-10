7e0311665b1556b98247f5e106086b4df9203a5d
Freakgod24 <dany.newby@gmail.com>
2025-01-10 17:19:53 -0500

    Feature: Add arbitrary number of glob arguments for MultiGrep

--- lua/freakgod/plugins/telescope.lua
+++ lua/freakgod/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
 				end,
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
-			{ "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		},
 		config = function()
 			require("telescope").setup({
@@ -39,14 +39,14 @@ return {
 			end
 
 			local live_multigrep = function(opts)
-				local pickers = require "telescope.pickers"
-				local finders = require "telescope.finders"
-				local make_entry = require "telescope.make_entry"
-				local conf = require "telescope.config".values
+				local pickers = require("telescope.pickers")
+				local finders = require("telescope.finders")
+				local make_entry = require("telescope.make_entry")
+				local conf = require("telescope.config").values
 				opts = opts or {}
 				opts.cwd = opts.cwd or vim.uv.cwd()
 
-				local finder = finders.new_async_job {
+				local finder = finders.new_async_job({
 					command_generator = function(prompt)
 						if not prompt or prompt == "" then
 							return nil
@@ -54,33 +54,45 @@ return {
 
 						local pieces = vim.split(prompt, "  ")
 						local args = { "rg" }
+
+						-- Add the first pieces as the regex to search in the files
 						if pieces[1] then
 							table.insert(args, "-e")
 							table.insert(args, pieces[1])
 						end
 
-						if pieces[2] then
+						-- Add all other pieces as globs.
+						for i = 2, #pieces do
 							table.insert(args, "-g")
-							table.insert(args, pieces[2])
+							table.insert(args, pieces[i])
 						end
 
 						---@diagnostic disable-next-line: deprecated
-						return vim.tbl_flatten {
+						return vim.tbl_flatten({
 							args,
-							{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
-						}
+							{
+								"--color=never",
+								"--no-heading",
+								"--with-filename",
+								"--line-number",
+								"--column",
+								"--smart-case",
+							},
+						})
 					end,
 					entry_maker = make_entry.gen_from_vimgrep(opts),
 					cwd = opts.cwd,
-				}
+				})
 
-				pickers.new(opts, {
-					debounce = 100,
-					prompt_title = "Multi Grep",
-					finder = finder,
-					previewer = conf.grep_previewer(opts),
-					sorter = require("telescope.sorters").empty(),
-				}):find()
+				pickers
+					.new(opts, {
+						debounce = 100,
+						prompt_title = "Multi Grep",
+						finder = finder,
+						previewer = conf.grep_previewer(opts),
+						sorter = require("telescope.sorters").empty(),
+					})
+					:find()
 			end
 
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
