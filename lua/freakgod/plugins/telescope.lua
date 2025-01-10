d0c887f7345224b7b1e1c4f208f18c60f3360cfe
DS\da097953 <dany.newby@mda.space>
2025-01-10 17:05:15 -0500

    Feature: Add a multigrep option in Telescope
    Taken from advent of nvim

--- lua/freakgod/plugins/telescope.lua
+++ lua/freakgod/plugins/telescope.lua
@@ -38,12 +38,58 @@ return {
 				end
 			end
 
+			local live_multigrep = function(opts)
+				local pickers = require "telescope.pickers"
+				local finders = require "telescope.finders"
+				local make_entry = require "telescope.make_entry"
+				local conf = require "telescope.config".values
+				opts = opts or {}
+				opts.cwd = opts.cwd or vim.uv.cwd()
+
+				local finder = finders.new_async_job {
+					command_generator = function(prompt)
+						if not prompt or prompt == "" then
+							return nil
+						end
+
+						local pieces = vim.split(prompt, "  ")
+						local args = { "rg" }
+						if pieces[1] then
+							table.insert(args, "-e")
+							table.insert(args, pieces[1])
+						end
+
+						if pieces[2] then
+							table.insert(args, "-g")
+							table.insert(args, pieces[2])
+						end
+
+						---@diagnostic disable-next-line: deprecated
+						return vim.tbl_flatten {
+							args,
+							{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+						}
+					end,
+					entry_maker = make_entry.gen_from_vimgrep(opts),
+					cwd = opts.cwd,
+				}
+
+				pickers.new(opts, {
+					debounce = 100,
+					prompt_title = "Multi Grep",
+					finder = finder,
+					previewer = conf.grep_previewer(opts),
+					sorter = require("telescope.sorters").empty(),
+				}):find()
+			end
+
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind All [F]iles" })
 			vim.keymap.set("n", "<leader>fF", TryGitFiles, { desc = "[F]ind Git [F]iles" })
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+			vim.keymap.set("n", "<leader>fG", live_multigrep, { desc = "[F]ind by Multi [G]rep" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
 			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "[F]ind [O]ld Files" })
